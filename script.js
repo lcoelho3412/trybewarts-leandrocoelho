717ade2734d22219dd9ee36b41f32265d8021003
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-05 17:55:43 -0300

    Adiciona requisito 3

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-//acessar button
-
+/* acessar button */
+const loginWork = document.querySelector(".trybewarts-login button")
+console.log(loginWork);
 //captura email
+loginWork.addEventListener('click', function () {
+  const emailReceiver = document.querySelector('.trybewarts-login').firstElementChild;
+  const passReceiver = document.querySelector('.trybewarts-login').firstElementChild.nextElementSibling;
+  const emailInput = emailReceiver.value;
+  const passInput = passReceiver.value;
+  console.log(emailInput, passInput);
+  if (emailInput === 'tryber@teste.com' && passInput === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
 //captura senha
 //envia ao clicar enviar
 //se email e senha bater alert Ola! 
