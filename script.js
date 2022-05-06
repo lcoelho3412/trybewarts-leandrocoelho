63ca80872fde866eef63f25f1fa907fa01e20abf
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-05-06 18:41:16 -0300

    concluido o requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const loginWork = document.querySelector('.trybewarts-login button');
-console.log(loginWork);
+/* constante para o botão de submit */
+const submitContent = document.querySelector('#submit-btn');
+/* constante para o checkbox agreement */
+const agreementChecked = document.getElementById('agreement');
 
 loginWork.addEventListener('click', () => {
   const emailReceiver = document.querySelector('.trybewarts-login').firstElementChild;
@@ -11,5 +14,17 @@ loginWork.addEventListener('click', () => {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+/* evento para habilitar ou desabilitar submit */
+
+agreementChecked.addEventListener('click', () => {
+  /* checkBox recebe o valor booleano do check agreement, caso marcado true, caso desmarcado false */
+  const checkBox = document.getElementById('agreement').checked;
+  /* confirma se o check agreement está marcado e habilita a função submit do botão */
+  if (checkBox === true) {
+    /* seta a propriedade disabled do botão para off(false) */
+    submitContent.disabled = false;
   }
 });
